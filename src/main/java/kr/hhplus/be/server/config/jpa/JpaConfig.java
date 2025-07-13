2289feb272b1aac318b38d88378f1f8340547b92
Heesom <heesom.hs@gmail.com>
2025-07-13 22:44:40 +0900

    기타: 설정에 대한 공부 주석

--- src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
+++ src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
@@ -8,7 +8,46 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
+
+/*
+ @CreatedDate, @LastModifiedDate, @CreatedBy, @LastModifiedBy 등의 애너테이션 사용
+*/
 @EnableJpaAuditing
+
+/*
+ 특정한 DataSource를 연결하고 싶을 경우
+
+@EnableJpaRepositories(
+    basePackages = "kr.hhplus.be.server.query.repository",
+    entityManagerFactoryRef = "queryEntityManagerFactory",
+    transactionManagerRef = "queryTransactionManager"
+)
+
+    @Bean(name = "queryDataSource")
+    @ConfigurationProperties("spring.datasource.query")
+    public DataSource queryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "queryEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean queryEntityManagerFactory(
+            @Qualifier("queryDataSource") DataSource dataSource
+            ) {
+
+        return new LocalContainerEntityManagerFactoryBean() {{
+            setDataSource(dataSource);
+            setPackagesToScan("kr.hhplus.be.server.query.entity");
+            setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        }};
+    }
+
+    @Bean(name = "queryTransactionManager")
+    public PlatformTransactionManager readTransactionManager(
+            @Qualifier("queryEntityManagerFactory") EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+ */
 @EnableJpaRepositories
 public class JpaConfig {
 
